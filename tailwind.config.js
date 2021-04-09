1b8b06196765d2de1ac95c3d566cb8aaf03fe401
Nik Spyratos <nik.spyratos@ankyr.dev>
2021-04-09 08:47:07 +0200

    WIP - new setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
-/*
- ** TailwindCSS Configuration File
- **
- ** Docs: https://tailwindcss.com/docs/configuration
- ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
- */
 module.exports = {
-  theme: {},
-  variants: {},
-  plugins: []
+    purge: {
+        mode: 'all',
+        content: [
+            './src/**/*.html'
+        ],
+        options: {
+            keyframes: true,
+        },
+    }
 }
