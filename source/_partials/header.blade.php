ad9598f2447ac20be884130037e304ef266a420d
Nik Spyratos <nik.spyratos@ankyr.dev>
2021-06-23 19:08:45 +0200

    Attempt #1 to fix macOS safari flex row element stretching

--- source/_partials/header.blade.php
+++ source/_partials/header.blade.php
@@ -1,5 +1,5 @@
 <header class="w-full p-4 flex flex-col lg:flex-row justify-between bg-tcg_dark border-tcg_pink border-b-2 rounded-t-xl text-tcg_white">
-    <div class="w-1/3 flex flex-row space-x-4">
+    <div class="w-1/3 flex flex-row items-start space-x-4">
         <img class="w-24" src="{{ $page->navigation->logo }}">
         <div class="flex flex-col self-center font-bold text-xl sm:text-2xl">
             <span class="text-tcg_light_blue">{{ $page->navigation->title }}</span>
